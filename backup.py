27c5b9c647ddb670a136e30b49bfee9461b05e67
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-05-07 00:38:41 +0100

    more

--- backup.py
+++ backup.py
@@ -1,1 +1,14 @@
+import os
+import easywebdav
+
+
 print("this is my script updated")
+print(os.environ['FTP_USER'])
+
+webdav = easywebdav.connect('asos-uk.custhelp.com', username=os.environ['FTP_USER'], password=os.environ['FTP_PASS'], protocol='https')
+list = webdav.ls("/dav/cp/customer/development")
+
+# print(list)
+exists = webdav.exists("/dav/cp/customer/development/models/custom/codmodel.php")
+if(exists): 
+	webdav.download("/dav/cp/customer/development/models/custom/codmodel.php", "/tmp/backups/UK/codmodel.php")
