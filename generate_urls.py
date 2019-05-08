6042e6101439224ef9c11d01760669bcae66ebcf
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-05-08 20:06:13 +0100

    pasoijdfasdf

--- generate_urls.py
+++ generate_urls.py
@@ -1,12 +1,27 @@
-def getInterfacesUrls():
-	return [
-	'asos.custhelp.com',
-	'asos-uk.custhelp.com', 
-	'asos-de.custhelp.com',
-	'asos-sp.custhelp.com',
-	'asos-it.custhelp.com',	
-	'asos-us.custhelp.com',
-	'asos-eu.custhelp.com',
-	'asos-cn.custhelp.com',
-	'asos-kr.custhelp.com',
-	'asos-se.custhelp.com']
+def getInterfacesUrls(interfacekey):
+
+	mapInterface = {
+		'com': 'asos.custhelp.com',
+		'uk': 'asos-uk.custhelp.com', 
+		'de': 'asos-de.custhelp.com',
+		'sp': 'asos-sp.custhelp.com',
+		'it': 'asos-it.custhelp.com',	
+		'us': 'asos-us.custhelp.com',
+		'eu': 'asos-eu.custhelp.com',
+		'cn': 'asos-cn.custhelp.com',
+		'kr': 'asos-kr.custhelp.com',
+		'se': 'asos-se.custhelp.com'
+	}
+
+	return mapInterface[interfacekey]
+
+
+
+def getPath(environment):
+	mapPath = {
+		'dev': '/dav/cp/customer/development/',
+		'stg': '/dav/cp/generated/staging/source/', 
+		'prod': '/dav/cp/generated/production/source/'		
+	}
+
+	return mapPath[environment]
--- read_file.py
+++ read_file.py
@@ -1,12 +1,29 @@
 import os
+import sys, getopt
 import easywebdav
+from generate_urls import *
 
 DOWNLOAD_CHUNK_SIZE_BYTES = 1 * 1024 * 1024
 
-webdav = easywebdav.connect('asos-uk.custhelp.com', username=os.environ['FTP_USER'], password=os.environ['FTP_PASS'], protocol='https')
+# Use case
+# read_file.py --interface=whatever --environment=dev --file=afile
+opts, _ = getopt.getopt(sys.argv[1:], [], ['interface=', 'environment=', 'file='])
 
-switchPath = "/dav/cp/customer/development/models/custom/UrlFactory.php"
+interface = ''
+environment = ''
+file = ''
+for opt, arg in opts:
+	if opt in ('--interface'):
+		interface = arg
+	if opt in ('--environment'):
+		environment = arg
+	if opt in ('--file'):
+		file = arg		
 
+
+webdav = easywebdav.connect(getInterfacesUrls(interface), username=os.environ['FTP_USER'], password=os.environ['FTP_PASS'], protocol='https')
+
+filePathInEnvironment = getPath(environment) + file
 
 def read(webdav, remote_path):	
     response = webdav._send('GET', remote_path, 200, stream=True)
@@ -16,5 +33,5 @@ def read(webdav, remote_path):
 
     return text
 
-filetext = read(webdav, switchPath)
+filetext = read(webdav, filePathInEnvironment)
 print(filetext)
